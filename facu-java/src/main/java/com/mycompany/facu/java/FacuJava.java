8d8cc502af665f7f71158f6cca2b314145253e57
Cauê Josê Oliveira Falcão <93996605+CaueJoseOliveiraFalcao@users.noreply.github.com>
2024-09-27 10:43:34 -0400

    example

---
+++ facu-java/src/main/java/com/mycompany/facu/java/FacuJava.java
@@ -0,0 +1,16 @@
+;/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.facu.java;
+
+/**
+ *
+ * @author cauej
+ */
+public class FacuJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
